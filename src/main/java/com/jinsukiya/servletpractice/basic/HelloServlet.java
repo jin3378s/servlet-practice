645649e53d5b2f8fa704c06f5e3686376e934b7d
jin3378s <jin3378s@gmail.com>
2022-03-25 00:40:32 +0900

    initialize spring boot for servlet practice

---
+++ src/main/java/com/jinsukiya/servletpractice/basic/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.jinsukiya.servletpractice.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        // message body
+        response.getWriter().write("hello : " + username);
+    }
+}
