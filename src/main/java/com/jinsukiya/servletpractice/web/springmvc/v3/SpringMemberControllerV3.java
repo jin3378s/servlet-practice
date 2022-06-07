e3bf4570dfc04702bcca94c3fd01bfff7e00ce12
jin3378s <jin3378s@gmail.com>
2022-06-07 23:22:24 +0900

    Feat: Spring Controller V3 변환.

--- src/main/java/com/jinsukiya/servletpractice/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/com/jinsukiya/servletpractice/web/springmvc/v3/SpringMemberControllerV3.java
@@ -1,9 +1,11 @@
-package com.jinsukiya.servletpractice.web.springmvc.v2;
+package com.jinsukiya.servletpractice.web.springmvc.v3;
 
 import com.jinsukiya.servletpractice.domain.member.Member;
 import com.jinsukiya.servletpractice.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletException;
@@ -13,36 +15,33 @@ import java.io.IOException;
 import java.util.List;
 
 @Controller
-@RequestMapping("/springmvc/v2/members")
-public class SpringMemberControllerV2 {
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
     @RequestMapping("/new-form")
-    public ModelAndView createForm() {
-        return new ModelAndView("new-form");
+    public String createForm() {
+            return "new-form";
     }
 
     @RequestMapping
-    public ModelAndView getMembers() throws ServletException, IOException {
+    public String getMembers(Model model) throws ServletException, IOException {
 
         List<Member> members = this.memberRepository.findAll();
-        ModelAndView mv = new ModelAndView("members");
-        mv.addObject("members",members);
 
-        return mv;
+        model.addAttribute("members",members);
+
+        return "members";
 
     }
     @RequestMapping("/save")
-    public ModelAndView createMember(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
+    public String createMember(@RequestParam("username") String username, @RequestParam("age")int age, Model model) throws ServletException, IOException {
 
         Member member = new Member(username,age);
         memberRepository.save(member);
 
-        ModelAndView mv = new ModelAndView("save-result");
-        mv.addObject("member",member);
+        model.addAttribute("member", member);
 
-        return mv;
+        return "save-result";
 
     }
 }
