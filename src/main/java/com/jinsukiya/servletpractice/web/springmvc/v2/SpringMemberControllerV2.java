80cd5f2057ef55463f76ecc7aab72f632089f91d
jin3378s <jin3378s@gmail.com>
2022-06-07 23:16:13 +0900

    Fix : common path 클래스 데코레이터로 분리

--- src/main/java/com/jinsukiya/servletpractice/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/com/jinsukiya/servletpractice/web/springmvc/v2/SpringMemberControllerV2.java
@@ -13,14 +13,15 @@ import java.io.IOException;
 import java.util.List;
 
 @Controller
+@RequestMapping("/springmvc/v2/members")
 public class SpringMemberControllerV2 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
-    @RequestMapping("/springmvc/v2/members/new-form")
+    @RequestMapping("/new-form")
     public ModelAndView createForm() {
         return new ModelAndView("new-form");
     }
 
-    @RequestMapping("/springmvc/v2/members")
+    @RequestMapping("/")
     public ModelAndView getMembers() throws ServletException, IOException {
 
         List<Member> members = this.memberRepository.findAll();
@@ -30,7 +31,7 @@ public class SpringMemberControllerV2 {
         return mv;
 
     }
-    @RequestMapping("/springmvc/v2/members/save")
+    @RequestMapping("/save")
     public ModelAndView createMember(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
